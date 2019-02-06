19411ff12f26a5a3170d51716816d650a6b658ee
ShubS <sharmashubh428@gmail.com>
2019-02-06 21:50:53 +0530

    updated pythagorean triplet

--- C/bruteForceForPyTriplets.c
+++ C/bruteForceForPyTriplets.c
@@ -1,11 +1,14 @@
 #include<stdio.h>
 int main(int argc, char const *argv[]) {
 double x,y,z,val;
+int n;
+printf("Enter a number\n");
+scanf("%d",&n);
   printf("\tPythagorean Triplets using Brute Force\n");
-  printf("Side 1\tSide 2\tHypotenuse" );
-  for(int i=1;i<=700;i++){
-    for(int j=1;j<=700;j++){
-      for(int k=1;k<=700;k++){
+  printf("Side 1\tSide 2\tHypotenuse\n" );
+  for(int i=1;i<=500;i++){
+    for(int j=1;j<=500;j++){
+      for(int k=1;k<=500;k++){
         val=j*j+i*i;
         if(k*k==val){
           printf("%.1d\t%.1d\t%d\n",i,j,k );
